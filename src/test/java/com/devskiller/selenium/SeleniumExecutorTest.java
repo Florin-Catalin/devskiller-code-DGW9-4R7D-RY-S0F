8bb20766a121abadf4dff48058d4c1f9ef1020bf
NipaphornOil <42369901+NipaphornOil@users.noreply.github.com>
2021-03-22 17:38:14 +0700

    .

---
+++ src/test/java/com/devskiller/selenium/SeleniumExecutorTest.java
@@ -0,0 +1,32 @@
+package com.devskiller.selenium;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.support.ui.Select;
+
+import static org.junit.Assert.assertEquals;
+
+public class SeleniumExecutorTest extends BaseSeleniumTest {
+
+    @Test
+    public void shouldInputValues() {
+        webDriver.get("http://localhost:8089/index.html");
+        SeleniumExecutor executor = new SeleniumExecutor(webDriver);
+
+        executor.getDropdownValues();
+//        executor.setInput("test input");
+
+
+        String[] inputs = new SeleniumExecutorExtension(webDriver).getInput();
+        assertEquals("test input", inputs[0]);
+        assertEquals("test input", inputs[1]);
+        assertEquals("test input", inputs[2]);
+
+        for (String var : inputs) {
+            System.out.println(var);
+        }
+
+        String[] dropDown = {"Volvo", "Fiat", "Opel", "Audi", "BMW", "Toyota"};
+        new SeleniumExecutorExtension(webDriver).setDropdownValues(dropDown);
+    }
+}
